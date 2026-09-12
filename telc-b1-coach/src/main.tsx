a0756595ef5bb35e567c4af7e8da548ffaea631e
Nicole Molina <nicole.andrea.molina.c@gmail.com>
2026-09-12 04:56:24 +0200

    Load expanded TELC exam corpus

--- telc-b1-coach/src/main.tsx
+++ telc-b1-coach/src/main.tsx
@@ -1,3 +1,3 @@
-import React from 'react';import{createRoot}from'react-dom/client';import App from'./App';import'./styles.css';import'./speakingTemplates';import'./vocabTopicFilter';
+import React from 'react';import{createRoot}from'react-dom/client';import App from'./App';import'./styles.css';import'./speakingTemplates';import'./vocabTopicFilter';import'./examCorpus';
 if('serviceWorker'in navigator)window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js').catch(()=>{}));
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
